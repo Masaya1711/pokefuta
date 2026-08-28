1c6861d252812891fec72cbf00b8d2296d03ee8f
kmtsjym <kmtsjym@gmail.com>
2026-08-28 23:17:06 +0900

    Point catalog URL to the real GitHub repo

--- ios/Sources/Services/AppConfig.swift
+++ ios/Sources/Services/AppConfig.swift
@@ -1,8 +1,7 @@
 import Foundation
 
 enum AppConfig {
-    /// リポジトリ作成後、実際のGitHubユーザー名/リポジトリ名に置き換えること。
     static let manholeCatalogURL = URL(
-        string: "https://raw.githubusercontent.com/kmtsjym/pokehuta/main/data/manholes.json"
+        string: "https://raw.githubusercontent.com/Masaya1711/pokefuta/master/data/manholes.json"
     )!
 }
